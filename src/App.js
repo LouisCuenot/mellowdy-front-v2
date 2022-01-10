f09fbe8d0304f45360c3c8b084bb01808b569922
kokorirko <gregoire.richard@hetic.net>
2022-01-10 15:33:51 +0100

    add route for fronttoken

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import Homepage from './Components/Homepage/Homepage';
 import About from './Components/About/About';
 import {BrowserRouter as Router, Route, Routes} from 'react-router-dom';
 import Navbar from './Components/Navbar/Navbar';
+import frontToken from './Components/frontToken';
 
 function App() {
   return (
@@ -15,6 +16,7 @@ function App() {
               <Route path="/" element={<Homepage />}/>
               <Route path="/about" element={<About />}/>
               <Route path='/generate-playlist' element={<GeneratePlaylist/>}/>
+              <Route path='/frontToken' element={<frontToken/>}/>
           </Routes>
         </div>
     </Router>
