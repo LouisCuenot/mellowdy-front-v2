42093257ccad919e456fe209feaa84cded59cba9
kokorirko <gregoire.richard@hetic.net>
2022-01-10 14:42:28 +0100

    update domain name

--- src/Components/frontToken.js
+++ src/Components/frontToken.js
@@ -6,7 +6,7 @@ function frontToken(){
     const queryParam = new URLSearchParams(window.location.search)
     const frontToken = queryParam.get('token')
     localStorage.setItem('FrontToken', frontToken == null ? "error" : frontToken) 
-    window.location.replace('https://pedantic-booth-c38e89.netlify.app/generate-playlist')
+    window.location.replace('https://mellowdy.fr/generate-playlist')
     const route = 'https://mellow-dy.tinker.ovh/'
     axios({ method: 'post', url: route, headers: { Authorization: `Bearer ${window.localStorage.getItem('FrontToken')}` } })
 
